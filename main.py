d8d07a94d0d618232a426726efa7dc589cacb856
Ruan Rafael <ruanrafaelleite@gmail.com>
2021-02-17 20:32:05 -0300

    Switched hash

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import json
-from requests import Session
+from requests import Session, utils
 from datetime import datetime
 from time import sleep
 
 LINK = 'https://www.instagram.com/accounts/login/'
 BASE_LINK = LINK + "ajax/"
-GRAPHQL_URL = "https://www.instagram.com/graphql/query/?query_hash=3dec7e2c57367ef3da3d987d89f9dbc8&variables="
+GRAPHQL_URL = "https://www.instagram.com/graphql/query/?query_hash=5aefa9893005572d237da5068082d8d5&variables="
+
 
 class User:
-    def __init__ (self, username, session):
+    def __init__(self, username, session):
         self.session = session
         self.username = username
-        self.userInfo = json.loads(self.session.get('https://www.instagram.com/' + username + '?__a=1').text)['graphql']['user']
+        self.userInfo = json.loads(self.session.get(
+            'https://www.instagram.com/' + username + '?__a=1').text)['graphql']['user']
         self.userId = self.userInfo['id']
 
-        self.graphql = '{"id":"' + self.userId + '","include_reel":true,"fetch_mutual":false,"first":10,"after":"QVFETVhMMlpsXzVONWdkWlhnWG9tNTY4TTFheWZjMDdmYXdISk5MUFVpMjgtNnd2QUVYOE5oLWd5NHBGckxia1djeTE1dXJHS0tOQnBtVjZtbVNfb05aSw=="}'
-        self.graphqlURL = GRAPHQL_URL + self.graphql
+        self.graphql = '{"id":"' + self.userId + '","include_reel":true,"fetch_mutual":false,"first":12,"after":"QVFETVhMMlpsXzVONWdkWlhnWG9tNTY4TTFheWZjMDdmYXdISk5MUFVpMjgtNnd2QUVYOE5oLWd5NHBGckxia1djeTE1dXJHS0tOQnBtVjZtbVNfb05aSw=="}'
+        self.graphqlURL = GRAPHQL_URL + utils.quote(self.graphql)
+
     def Followers(self):
         req = self.session.get(self.graphqlURL)
-        edges = json.loads(req.text)['data']['user']['edge_follow']['edges']
-        
+        print(req.text)
+
+        edges = json.loads(req.text)
+
         print(len(edges))
         for node in edges:
             print(node)
 
 
 class Requests():
-    def __init__(self):#Inicia a session e pega o token inicial
+    def __init__(self):  # Inicia a session e pega o token inicial
         self.session = Session()
         self.session.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
@@ -35,9 +40,10 @@ class Requests():
             'X-Requested-With': 'XMLHttpRequest'
         }
         self.GetToken(LINK)
-        self.session.headers.update({'Referer': LINK, 'X-CSRFToken': self.token})
+        self.session.headers.update(
+            {'Referer': LINK, 'X-CSRFToken': self.token})
 
-    def GetToken(self, url):#atualiza o token
+    def GetToken(self, url):  # atualiza o token
         req = self.session.get(url)
         self.token = req.cookies['csrftoken']
 
@@ -57,15 +63,15 @@ class Requests():
         return self.session
 
 
-
-username = "bot"
-password = "botter"
+username = "teste"
+password = "teste"
 
 BotReq = Requests()
 session = BotReq.MakeLogin(username, password)
 
-ScrapingUser = User("perf", session)
+ScrapingUser = User("perfil", session)
 ScrapingUser.Followers()
+
 
 """ 
 wordlist = []
